e1d4e1e8e5892849810553c929ad7daa72386ac3
DorianContal <dorian.contal@epitech.eu>
2023-11-10 13:27:59 +0100

    Interface Start crée, avec desja quelque assets

--- Fishing_Game/demo/src/main/java/fishingame/App.java
+++ Fishing_Game/demo/src/main/java/fishingame/App.java
@@ -1,10 +1,25 @@
 package fishingame;
 
+import javafx.animation.ScaleTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.geometry.Pos;
+import javafx.scene.layout.VBox;
+import javafx.animation.ScaleTransition;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -14,21 +29,78 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    private static Pane pane;
+
+    private void setupButtonInteraction(Button button) {
+    ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(200), button);
+
+    button.setOnMouseEntered(event -> {
+        scaleTransition.setFromX(1);
+        scaleTransition.setFromY(1);
+        scaleTransition.setToX(1.2);
+        scaleTransition.setToY(1.2);
+        scaleTransition.play();
+    });
+
+    button.setOnMouseExited(event -> {
+        scaleTransition.setFromX(1.2);
+        scaleTransition.setFromY(1.2);
+        scaleTransition.setToX(1);
+        scaleTransition.setToY(1);
+        scaleTransition.play();
+    });
+}
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        Image backgroundImage = new Image("background.png");
+        ImageView backgroundImageView = new ImageView(backgroundImage);
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().add(backgroundImageView);
+        scene = new Scene(stackPane, 1280, 720);
+
+        Rectangle MenuPrincipal = new Rectangle(100, 100, 640, 360);
+        ImagePattern pattern = new ImagePattern(new Image("Tableau.png"));
+        MenuPrincipal.setFill(pattern);
+        stage.setTitle("Fishing Supremacy");
+
+        
+
+        VBox buttonBox = new VBox(20);
+        buttonBox.setAlignment(Pos.CENTER);
+
+        Button ButtonStart = createButton("Start.png");
+        Button ButtonExit = createButton("Exit.png");
+
+        setupButtonInteraction(ButtonStart);
+        setupButtonInteraction(ButtonExit);
+
+        ButtonStart.setOnAction(event -> {
+            MenuPrincipal.setVisible(false);
+            buttonBox.setVisible(false);
+        });
+
+        ButtonExit.setOnAction(event -> {
+            stage.hide();
+        });
+
+        buttonBox.getChildren().addAll(ButtonStart, ButtonExit);
+        stackPane.getChildren().addAll(MenuPrincipal, buttonBox);
+
+
+
         stage.setScene(scene);
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+    private Button createButton(String imagePath) {
+        Image image = new Image(imagePath);
+        ImageView imageView = new ImageView(image);
+        Button button = new Button();
+        button.setGraphic(imageView);
+        button.setStyle("-fx-background-color: transparent;");
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+        return button;
     }
 
     public static void main(String[] args) {
