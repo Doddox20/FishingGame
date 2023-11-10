88c27dbc22a8d213155fc347d45a605706d4357f
DorianContal <dorian.contal@epitech.eu>
2023-11-10 14:40:39 +0100

    Ajout label Press Start qui s'affiche apres click start

--- Fishing_Game/demo/src/main/java/fishingame/App.java
+++ Fishing_Game/demo/src/main/java/fishingame/App.java
@@ -1,6 +1,9 @@
 package fishingame;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
 import javafx.animation.ScaleTransition;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -14,11 +17,9 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.geometry.Pos;
-import javafx.scene.layout.VBox;
-import javafx.animation.ScaleTransition;
 import javafx.util.Duration;
 
 import java.io.IOException;
@@ -63,7 +64,7 @@ public class App extends Application {
         ImagePattern pattern = new ImagePattern(new Image("Tableau.png"));
         MenuPrincipal.setFill(pattern);
         stage.setTitle("Fishing Supremacy");
-
+        
         
 
         VBox buttonBox = new VBox(20);
@@ -75,17 +76,39 @@ public class App extends Application {
         setupButtonInteraction(ButtonStart);
         setupButtonInteraction(ButtonExit);
 
+        
+
+
+        Label labelStartClick = new Label("Press SPACE to Fish !");
+        labelStartClick.setStyle("-fx-font-size: 50; -fx-text-fill: Black;");
+
+        labelStartClick.setVisible(false);
+
+        
+        
+
         ButtonStart.setOnAction(event -> {
             MenuPrincipal.setVisible(false);
             buttonBox.setVisible(false);
+            labelStartClick.setVisible(true);
+
+
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(0.5), new KeyValue(labelStartClick.visibleProperty(), false)),
+                new KeyFrame(Duration.seconds(1), new KeyValue(labelStartClick.visibleProperty(), true))
+        );
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+
+
         });
 
         ButtonExit.setOnAction(event -> {
             stage.hide();
         });
 
-        buttonBox.getChildren().addAll(ButtonStart, ButtonExit);
-        stackPane.getChildren().addAll(MenuPrincipal, buttonBox);
+        buttonBox.getChildren().addAll(ButtonStart, ButtonExit );
+        stackPane.getChildren().addAll(MenuPrincipal, buttonBox, labelStartClick );
 
 
 
